06e432f5177031ab9f718467c4dc78fa2dff6313
quixoticdicker <cdicker@lclark.edu>
2014-06-18 10:33:41 -0700

    added a function that returns a random float from 0-1

--- prototype.cu
+++ prototype.cu
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <curand.h>
+
+#define POP_PER_ISLAND 1024
+#define NUM_ISLANDS 7
+
 __global__ void evolve(Individual* pop, Individual* boat)
 {
 	__shared__ Individual oldPop[POP_PER_ISLAND];
@@ -7,16 +15,18 @@ __global__ void evolve(Individual* pop, Individual* boat)
 	oldPop[threadIdx.x] = pop[threadIdx.x + blockIdx.x * blockDim.x];
 	boat[blockIdx.x] = oldPop[0];
 	
+	curand_init(
+	
 	for(g = 0; g < 100; g++)
 	{
-		fitness(oldPop[threadIdx.x]);
+		evaluate(oldPop[threadIdx.x]);
 		
-		int a = rand() % POP_PER_ISLAND;
-		int b = rand() % POP_PER_ISLAND;
-		newPop[threadIdx.x] = evaluation(oldPop[a], oldPop[b]);
+		int a = pharaohRand() * RAND_MAX % POP_PER_ISLAND;
+		int b = pharaohRand() * RAND_MAX % POP_PER_ISLAND;
+		newPop[threadIdx.x] = arena(oldPop[a], oldPop[b]);
 		
 		mutate(newPop[threadIdx.x]);
-		crossover(newPop[threadIdx.x]);
+		//crossover(newPop[threadIdx.x]);
 
 		boat[(bockIdx.x + 1) % NUM_ISLANDS] = newPop[0];
 		__syncthreads()
@@ -28,7 +38,7 @@ __global__ void evolve(Individual* pop, Individual* boat)
 	}
 }
 
-__device__ Individual evaluation(Individual a, Individual b)
+__device__ Individual arena(Individual a, Individual b)
 {
 	if(a.fitness > b.fitness)
 	{
@@ -38,4 +48,22 @@ __device__ Individual evaluation(Individual a, Individual b)
 	{
 		return b;
 	}
+}
+
+__device__ float pharaohRand()
+{
+	int seed = clock();
+	curandState_t myState;
+	curand_init(seed, blockIdx.x * blockDim.x, threadIdx.x, &myState);
+	
+	return curand_uniform(curandState_t);
+}
+
+int main void()
+{
+	Individual* pop;
+	Individual* d_pop;
+	
+	
+	<<<NUM_ISLANDS, POP_PER_ISLAND>>> evolve(d_pop, d_boat);
 }
